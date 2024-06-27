15b415b8e536b44d00b47c6e2a8c8a1e60801781
babin78 <arijit.newgen@gmail.com>
2024-06-27 14:39:37 +0530

    done

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputval = document.querySelector("#filter-water");
+const btn = document.querySelector("#calc");
+const outputText = document.querySelector("#output");
+const outval = document.querySelector("#outval");
+
+/*
+document.onload(() => {
+  outval.innerHTML = "";
+  outputText.innerHTML = "";
+});
+*/
+
+btn.addEventListener("click", () => {
+  outval.innerHTML = "";
+  outputText.innerHTML = "";
+  console.log(inputval.value);
+  if (inputval.value === undefined) {
+    outputText.innerHTML = "Please enter a numeric value";
+  } else if (inputval.value <= 0) {
+    outputText.innerHTML = "Please enter a non zero value";
+  } else {
+    console.log(inputval.value * 3);
+    outputText.innerHTML = `output: ${inputval.value * 3}`;
+  }
+
+  //document.getElementById("#output").  .style.display = "inline-block";
+});
+
+//btn.click();
